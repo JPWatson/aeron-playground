1c571318e5b871f13fdd7c0fb7c1346341f2d2ed
James Watson <james@weareadaptive.com>
2019-10-04 08:04:12 +0100

    Add entry points

--- code/src/main/java/com/weareadaptive/hydra/training/aeron/Publisher.java
+++ code/src/main/java/com/weareadaptive/hydra/training/aeron/Publisher.java
@@ -2,9 +2,11 @@ package com.weareadaptive.hydra.training.aeron;
 
 import org.agrona.concurrent.ShutdownSignalBarrier;
 
-public class BasicPublisher {
+public class Publisher {
   public static void main(String[] args) {
-    System.out.println("Started Publisher");
+    final String channel = System.getProperty("channel", "aeron:udp?endpoint=localhost:9000");
+
+    System.out.println("Started Publisher on " + channel);
 
     // TODO
 
--- code/src/main/java/com/weareadaptive/hydra/training/aeron/Subscriber.java
+++ code/src/main/java/com/weareadaptive/hydra/training/aeron/Subscriber.java
@@ -2,9 +2,12 @@ package com.weareadaptive.hydra.training.aeron;
 
 import org.agrona.concurrent.ShutdownSignalBarrier;
 
-public class BasicSubscriber {
+public class Subscriber {
   public static void main(String[] args) {
-    System.out.println("Started Subscriber");
+    final String channel = System.getProperty("channel", "aeron:udp?endpoint=localhost:9000");
+
+    System.out.println("Started Subscriber on " + channel);
+
 
     // TODO
 
